4557591224b6f7b23c5cffd85eaaec6abbc22ac7
Milan Jaric <milan.jaric@gmail.com>
2020-11-25 14:34:25 +0100

    poc, send header before ssl hello record

--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,4 @@
 use Mix.Config
+
+config :tds,
+  opts: [hostname: "nitrox", username: "sa", password: "some!Password", database: "test", ssl: true, ssl_opts: [certfile: "/Users/mjaric/prj/github/tds/mssql.pem", keyfile: "/Users/mjaric/prj/github/tds/mssql.key"]]
